4823967ffd56e71d556c3ada5825f7986f23fb32
Whiterun <winterhold89@gmail.com>
2018-09-19 23:30:16 +0700

    #

--- class/Request.php
+++ class/Request.php
@@ -1,36 +1,95 @@
 ﻿<?php
-Class Request {
-	function __construct()
+	Class Request
 	{
-		// $this->uri		= 'https://api.turbo.co.id/';
-		$this->uri		= 'localhost:8888/';
-		$this->query	= [ 'secret_key' => '4510deb00627a4dabb800d0f485af867f4e836e7' ];
-		$this->client	= new GuzzleHttp\Client();
-	}
-	
-	public function get( $method, $query = [])
-	{
-		$collection = $this->query;
+		protected $tokenInfo = [];
 		
-		if ( !empty($query) )
+		function __construct()
 		{
-			$collection = array_merge($collection, $query);
+			// $this->uri		= 'https://api.turbo.co.id/';
+			$this->uri		= 'localhost:8888/';
+			$this->client	= new GuzzleHttp\Client();
+			$this->query	= [ 'secret_key' => '4510deb00627a4dabb800d0f485af867f4e836e7' ];
+			
+			$this->cbb1		= 'http://www.chinabuybuy.com/api/rest_admin/';
+			$this->cbb2		= 'http://www.chinabuybuy.com/index.php?route=rest/';
+			$this->headers	= [ 'Accept' => 'application/json', 'Content-Type' => 'application/json' ];
 		}
 		
-		$response = $this->client->get( $this->uri . $method, [ 'query' => $collection ]);
-		return $response->getBody();
+		public function get( $method, $query = [])
+		{
+			$collection = $this->query;
+			
+			if ( !empty($query) )
+			{
+				$collection = array_merge($collection, $query);
+			}
+			
+			$response = $this->client->get( $this->uri . $method, [ 'query' => $collection ]);
+			return $response->getBody();
+		}
+		
+		public function post( $method, $params = [])
+		{
+			$response = $this->client->post( $this->uri . $method . '?' . http_build_query($this->query), [
+				'headers'		=> [
+					'Accept'		=> 'application/json',
+					'Content-Type'	=> 'application/json'
+				],
+				'json' => $params
+			]);
+			// return $response->getBody();
+		}
+		
+		public function cbbAuthorize()
+		{
+			// Use Basic Token to authorize and get Authorization Bearer
+			$response	= $this->client->post($this->cbb1.'oauth2/token/client_credentials', [
+				'headers' => [
+					'Authorization' => 'Basic ' . $this->cbbToken()
+				]
+			]);
+			
+			// Save the auth_bearer to reuse it in every requests
+			$this->saveCbbToken(json_decode((string) $response->getBody(), true));
+			
+			// Logging in using specific user
+			$response	= $this->client->post($this->cbb1.'login', [
+				'headers'	=> $this->authHeaders(),
+				'json'		=> [
+					'username' => 'restadmin',
+					'password' => '123456'
+				]
+			]);
+			
+			return json_decode((string) $response->getBody(), true);
+		}
+		
+		protected function cbbToken()
+		{
+			return base64_encode('matahari:m4t4h4r1899');
+		}
+		
+		protected function saveCbbToken($response)
+		{
+			$this->tokenInfo = $response;
+		}
+		
+		protected function authHeaders()
+		{
+			return array_merge( $this->headers, [
+				'Authorization'	=> 'Bearer ' . $this->tokenInfo['access_token']
+			]);
+		}
+		
+		public function product()
+		{
+			$response	= $this->client->get($this->cbb1.'products/94761', [
+				'headers'	=> $this->authHeaders()
+			]);
+			
+			$data = str_replace([ "\n", "\r" ], [ ' ', ' ' ], $response->getBody());
+			
+			return json_decode((string) $data, true);
+		}
 	}
-	
-	public function post( $method, $params = [])
-	{
-		$response = $this->client->post( $this->uri . $method . '?' . http_build_query($this->query), [
-			'headers'		=> [
-				'Accept'		=> 'application/json',
-				'Content-Type'	=> 'application/json'
-			],
-			'json' => $params
-		]);
-		// return $response->getBody();
-	}
-}
 ?>
--- test.php
+++ test.php
@@ -1,7 +1,56 @@
 ﻿<?php
 	require('start.php');
 	
-	$params = $_GET;
+	$request->cbbAuthorize();
 	
-	echo $request->get('all_produk', $params);
+	$product = $request->product()['data'];
+	
+	$parsed = [];
+	foreach ( $product['options'] as $key )
+	{
+		$option = [];
+		foreach ( $key['option_value'] as $value )
+		{
+			$sku		= $key['option_id'].$value['option_value_id'];
+			$option[]	= array_merge( $value, [ 'key' => ucfirst($key['name']), 'sku' => $sku ]);
+		}
+		
+		$parsed[] = $option;
+	}
+	
+	function combinations($product_id, $arrays, $i = 0)
+	{
+		if ( !isset($arrays[$i]) ) return array();
+		
+		if ( $i == count($arrays) - 1 ) return $arrays[$i];
+		
+		// get combinations from subsequent arrays
+		$tmp = combinations($product_id, $arrays, $i + 1);
+
+		$result = [];
+		
+		// concat each array from tmp with each element from $arrays[$i]
+		foreach ( $arrays[$i] as $v )
+		{
+			foreach ( $tmp as $t )
+			{
+				$result[] = is_array($t) ? 
+					[
+						'my_product_id'	=> $product_id,
+						'sku'			=> $v['sku'].$product_id.$t['sku'], 
+						'attributes'	=> json_encode([
+							[ 'name' => $v['key'],'option' => $v['name'] ],
+							[ 'name' => $t['key'],'option' => $t['name'] ]
+						], 1)
+					] :
+					array($v, $t);
+			}
+		}
+
+		return $result;
+	}
+
+	print_r(
+		combinations($product['id'], $parsed)
+	);
 ?>
